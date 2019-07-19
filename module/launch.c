39cd075cce69de9dbf7d1198bd30cb5ce69fbd8d
Scott Pakin <scott+gpg@pakin.org>
2019-07-19 00:20:31 -0600

    Create but don't bind the Apache-side socket

--- module/launch.c
+++ module/launch.c
@@ -6,17 +6,14 @@
 
 #include "gosp.h"
 
-/* Create and bind a Unix-domain stream socket to associate with a given Go
- * Server Page.  Create all directories needed along the way.  Return NULL on
- * failure. */
+/* Create a socket to associate with a given Go Server Page.  Create all
+ * directories needed along the way.  Return NULL on failure. */
 apr_socket_t *create_socket(request_rec *r, const char *run_dir)
 {
   apr_socket_t *sock;   /* The socket proper */
-  apr_sockaddr_t *sa;   /* Socket address information, used for binding the socket */
   char *sock_name;      /* Socket filename */
   char *sock_dir;       /* Directory component of the socket filename */
   char *idx;            /* Pointer into a string */
-  apr_finfo_t finfo;    /* File information */
   apr_status_t status;  /* Status of an APR call */
 
   /* Construct a socket name from the Gosp filename. */
@@ -43,17 +40,5 @@ apr_socket_t *create_socket(request_rec *r, const char *run_dir)
   if (status != APR_SUCCESS)
     REPORT_ERROR(NULL, APLOG_ALERT, status,
                  "Failed to create socket %s", sock_name);
-
-  /* Populate the socket-address information. */
-  status = apr_sockaddr_info_get(&sa, sock_name, APR_UNIX, 0, 0, r->pool);
-  if (status != APR_SUCCESS)
-    REPORT_ERROR(NULL, APLOG_ALERT, status,
-                 "Failed to acquire address information for socket %s", sock_name);
-
-  /* Bind the socket. */
-  status = apr_socket_bind(sock, sa);
-  if (status != APR_SUCCESS)
-    REPORT_ERROR(NULL, APLOG_ALERT, status,
-                 "Failed to bind socket %s", sock_name);
   return sock;
 }
